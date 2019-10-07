208d0df932c7d5063e12b7251bc33cff29b7a7ed
Jony Hayama <jonathan.hayama@gmail.com>
2019-10-07 08:36:25 -0300

    more clients added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   /* Accordion */
   var accordion_titles = document.querySelectorAll('.accordion > .accordion-title');
-  for( var i = 0, tab; title = accordion_titles[i]; i++ ){
+  for( var i = 0, title; title = accordion_titles[i]; i++ ){
     title.addEventListener('click', function(e){
       var isCurrentItem = this.classList.contains('active');
       var other_divs = document.querySelectorAll('.accordion > .accordion-title.active');
@@ -97,4 +97,34 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   }
+
+  /* More Clients */
+  function resizeSlider(){
+    var height = document.querySelector('#more-clients .slider__contents:first-child > ul').offsetHeight;
+    document.querySelector('#more-clients > .slider').style.height = (height + 26) + 'px';
+  }
+  var more_clients = document.querySelectorAll('#more-clients > ul > li');
+  var clients = document.getElementById('more-clients');
+  var chunk = clients.getAttribute('data-per-page');
+  var style = document.createElement('style');
+  var slides_html = '';
+  var radios_html = '';
+  clients.innerHTML = '';
+  for (i = 0, j = more_clients.length, count = 1; i < j; i += chunk, count++ ) {
+    var lis = Array.prototype.slice.call( more_clients, i, i+chunk);
+    var checked = (count == 1) ? 'checked="checked"' : '';
+    var left = (count == 1) ? 0 : ( ((count - 1) * 100 ) * -1 );
+    radios_html = radios_html + '<input type="radio" name="slider" title="slide' + count + '" ' + checked + ' class="slider__nav"/>';
+    slides_html = slides_html + '<div class="slider__contents"><ul class="gallery gallery-columns-' + chunk + '">' + lis.map(function(elem){ return elem.outerHTML }).join('') + '</ul></div>';
+    style.innerHTML += '.slider__nav:checked:nth-of-type(' + count + ') ~ .slider__inner { left: ' + left + '%; }';
+  }
+  document.head.appendChild(style);
+
+  clients.innerHTML = '<div class="slider">' + radios_html + '<div class="slider__inner">' + slides_html + '</div></div>';
+  resizeSlider();
+  
+  window.addEventListener('resize', function(){
+    resizeSlider();
+  });
+
 });
